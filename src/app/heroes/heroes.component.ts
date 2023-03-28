7030c61e54b3bc071c83782427da66bb997adc6b
Akshay-Deodhar <akshaynd95@gmail.com>
2023-03-28 11:42:26 +0530

    Added separate component and services.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../heroes-list';
+
+import { HeroService } from './hero.service';
+import { MessageService } from '../messages/messages.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +12,31 @@ import { HEROES } from '../heroes-list';
 })
 
 export class HeroesComponent {
+
+  heroes: Hero[] = [];
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-
-  heroes = HEROES;
   selectedHero?: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
